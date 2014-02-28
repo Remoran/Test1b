6d1571421892585ac61bf0b537d7e6efe693f9ca
Ryan <ryan@Ryan-Morans-Macbook-Pro.local>
2014-02-27 19:01:17 -0500

    Initial Commit

---
+++ Test1b/Test1bAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Test1bAppDelegate.h
+//  Test1b
+//
+//  Created by Ryan on 2/27/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Test1bAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test1b/Test1bViewController.h
@@ -0,0 +1,13 @@
+//
+//  Test1bViewController.h
+//  Test1b
+//
+//  Created by Ryan on 2/27/14.
+//  Copyright (c) 2014 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Test1bViewController : UIViewController
+
+@end
